575b09cc1e602abbc34cb25eb945eb998a7f2cff
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-01-22 10:04:34 -0300

    chore: update nvim configs

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -343,19 +343,19 @@ M.nvterm = {
     },
 
     -- new
-    ["<leader>h"] = {
-      function()
-        require("nvterm.terminal").new "horizontal"
-      end,
-      "New horizontal term",
-    },
+    -- ["<leader>h"] = {
+    --   function()
+    --     require("nvterm.terminal").new "horizontal"
+    --   end,
+    --   "New horizontal term",
+    -- },
 
-    ["<leader>v"] = {
-      function()
-        require("nvterm.terminal").new "vertical"
-      end,
-      "New vertical term",
-    },
+    -- ["<leader>v"] = {
+    --   function()
+    --     require("nvterm.terminal").new "vertical"
+    --   end,
+    --   "New vertical term",
+    -- },
   },
 }
 
