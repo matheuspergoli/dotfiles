f1d1865cb4dd5e60874ef56ad96b2354cdb38a53
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-01-30 13:18:12 -0300

    chore: add scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,14 @@
 sudo pacman -Sy archlinux-keyring &&
 sudo pacman -Syu &&
 sudo pacman -S git base-devel curl &&
-sudo pacman -S neovim &&
 sudo pacman -S zsh zsh-completions &&
+sudo pacman -S tmux neovim docker &&
 
+sudo groupadd docker &&
+sudo usermod -aG docker $USER &&
+newgrp docker &&
+
+sudo systemctl start docker.service &&
+sudo systemctl enable docker.service &&
+
+touch ~/.zshrc &&
