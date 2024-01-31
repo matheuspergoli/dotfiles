3a42a7f4b38d2fa72ec822d9a3a34a7f23f279dc
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-01-31 19:49:11 -0300

    chore: add more configs

---
+++ scripts/packages.sh
@@ -0,0 +1,31 @@
+# Instalando pacotes iniciais
+sudo pacman -S git base-devel curl tmux neovim zsh docker docker-compose unzip neofetch &&
+
+# Configurando o docker
+sudo groupadd docker &&
+sudo usermod -aG docker $USER && newgrp docker &&
+sudo systemctl enable docker.service &&
+sudo systemctl start docker.service &&
+
+# Instalando oh-my-zsh e plugins
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k &&
+
+# Instalando nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash &&
+
+# Clonando meus dotfiles e configurando nvim/tmux e zsh
+git clone https://github.com/matheuspergoli/dotfiles.git ~/.dotfiles && mkdir ~/.config && cp -r ~/.dotfiles/.config/** ~/.config && cp -r ~/.dotfiles/.zshrc ~/.zshrc &&
+
+# Instalando tpm
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm &&
+
+source ~/.zshrc &&
+
+# Configurando node e instalando pnpm
+nvm install node && corepack enable && corepack enable pnpm &&
+
+rm -rf ~/.dotfiles &&
+
+echo "Zap"
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,50 +1,22 @@
-# Configurando arch
+# Apagando arquivos do pacman e gnupg para evitar erros de chave pública inválida
+rm -rf ~/.gnupg &&
+rm -rf /var/cache/pacman &&
+rm -rf /etc/pacman.d/gnupg &&
+
+# Configurando chaves do pacman e atualizando o sistema
 pacman-key --init &&
 pacman-key --populate &&
 pacman -Sy archlinux-keyring &&
 pacman -Su &&
 pacman -Syyu &&
 
-# Setup inicial após configuração do arch
-#
 # Configurando sudoers file
-# echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
-#
+echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel &&
+
 # Criando usuário
-# useradd -m -G wheel -s /bin/bash heizmen
-#
+useradd -m -G wheel -s /bin/bash heizmen &&
+
 # Definindo senha
-# passwd heizmen
-# 
-# exit para entrar com o usuário criado no wsl
-
-# Instalando pacotes iniciais
-sudo pacman -S git base-devel curl tmux neovim zsh docker docker-compose unzip neofetch &&
-
-# Configurando o docker
-sudo groupadd docker &&
-sudo usermod -aG docker $USER && newgrp docker &&
-sudo systemctl enable docker.service &&
-sudo systemctl start docker.service &&
-
-# Instalando oh-my-zsh e plugins
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k &&
-
-# Instalando nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash &&
-
-# Clonando meus dotfiles e configurando nvim/tmux e zsh
-git clone https://github.com/matheuspergoli/dotfiles.git ~/.dotfiles && mkdir ~/.config && cp -r ~/.dotfiles/.config/** ~/.config && cp -r ~/.dotfiles/.zshrc ~/.zshrc &&
-
-# Instalando tpm
-git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm &&
-
-# Configurando node e instalando pnpm
-nvm install node && corepack enable && corepack enable pnpm &&
-
-rm -rf ~/.dotfiles &&
-source ~/.zshrc &&
+passwd heizmen &&
 
 echo "Zap"
