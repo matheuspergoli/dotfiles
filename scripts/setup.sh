dc15ac99497745993362432be2937d4da28d3df1
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-01-31 15:16:35 -0300

    chore: configs

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,20 +13,18 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k &&
 
-# Instalando tpm
-git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm/ &&
-
 # Instalando o nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash &&
 
 # Clonando meus dotfiles e configurando nvim/tmux e zsh
 git clone https://github.com/matheuspergoli/dotfiles.git ~/.dotfiles && mkdir ~/.config && cp -r ~/.dotfiles/.config/** ~/.config && cp -r ~/.dotfiles/.zshrc ~/.zshrc &&
 
+# Instalando tpm
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm &&
+
+# Configurando node e instalando pnpm
+nvm install node && corepack enable && corepack enable pnpm &&
+
 rm -rf ~/.dotfiles &&
 source ~/.zshrc &&
 source ~/.config/tmux/tmux.conf &&
-
-# Configurando node e instalando pnpm
-nvm install node && corepack enable && corepack install pnpm &&
-
-
