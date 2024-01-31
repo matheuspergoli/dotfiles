28fb19cc3da067056debe0726ce96c4ed0d29e07
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-01-31 14:07:34 -0300

    chore: add zsh config

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,19 @@
-sudo pacman -Sy archlinux-keyring &&
 sudo pacman -Syu &&
-sudo pacman -S git base-devel curl &&
-sudo pacman -S zsh zsh-completions &&
-sudo pacman -S tmux neovim docker &&
+sudo pacman -S git base-devel curl tmux neovim zsh docker unzip neofetch &&
 
 sudo groupadd docker &&
-sudo usermod -aG docker $USER &&
-newgrp docker &&
+sudo usermod -aG docker $USER && newgrp docker &&
 
-sudo systemctl start docker.service &&
 sudo systemctl enable docker.service &&
+sudo systemctl start docker.service &&
 
-touch ~/.zshrc &&
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k &&
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash &&
+
+git clone https://github.com/matheuspergoli/dotfiles.git &&
+
